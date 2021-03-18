535cefe240c96d2742e58c98410c4c9e673401d4
alifudin-a <alifudinalfarizi@gmail.com>
2021-03-18 17:35:07 +0700

    add route

---
+++ route/route.go
@@ -0,0 +1,31 @@
+package route
+
+import (
+	"net/http"
+
+	action "github.com/alifudin-a/arsip-surat-puskom/action/jabatan-struktural"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func InitRoute() *echo.Echo {
+	e := echo.New()
+
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "==> METHOD=${method}, URI=${uri}, STATUS=${status}, " +
+			"HOST=${host}, ERROR=${error}, LATENCY_HUMAN=${latency_human}\n",
+	}))
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+		AllowMethods: []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
+	}))
+
+	api := e.Group("/api")
+	api.GET("/jabatan_struktural", action.ListJabatanStrukturalHandler)
+
+	e.Logger.Fatal(e.Start(":9000"))
+
+	return e
+}
