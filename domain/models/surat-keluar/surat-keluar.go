32d91449b64323e078cf7c552b6180b77c05ea45
alifudin-a <alifudinalfarizi@gmail.com>
2021-03-22 13:50:36 +0700

    perbaikan fitur surat keluar

--- domain/models/surat-keluar/surat-keluar.go
+++ domain/models/surat-keluar/surat-keluar.go
@@ -5,7 +5,6 @@ type SuratKeluar struct {
 	ID         int64   `json:"id" db:"id"`
 	Tanggal    string  `json:"tanggal" db:"tanggal" validate:"required"`
 	Nomor      string  `json:"nomor" db:"nomor" validate:"required"`
-	IDPenerima int64   `json:"id_penerima" db:"id_penerima" validate:"required"`
 	IDPengirim int64   `json:"id_pengirim" db:"id_pengirim" validate:"required"`
 	Perihal    string  `json:"perihal" db:"perihal" validate:"required"`
 	IDJenis    *int64  `json:"id_jenis" db:"id_jenis"`
@@ -25,6 +24,19 @@ type ListSuratKeluar struct {
 	Perihal  string `json:"perihal" db:"perihal" validate:"required"`
 	// IDJenis    *int64  `json:"id_jenis" db:"id_jenis"`
 	Jenis      *string `json:"jenis" db:"jenis"`
+	Keterangan *string `json:"keterangan" db:"keterangan"`
+	CreatedAt  *string `json:"created_at,omitempty" db:"created_at"`
+	UpdatedAt  *string `json:"updated_at,omitempty" db:"updated_at"`
+}
+
+// SuratKeluar model struct
+type CreateSuratKeluar struct {
+	ID         int64   `json:"id" db:"id"`
+	Tanggal    string  `json:"tanggal" db:"tanggal" validate:"required"`
+	Nomor      string  `json:"nomor" db:"nomor" validate:"required"`
+	IDPengirim int64   `json:"id_pengirim" db:"id_pengirim" validate:"required"`
+	Perihal    string  `json:"perihal" db:"perihal" validate:"required"`
+	IDJenis    *int64  `json:"-" db:"id_jenis"`
 	Keterangan *string `json:"keterangan" db:"keterangan"`
 	CreatedAt  *string `json:"created_at,omitempty" db:"created_at"`
 	UpdatedAt  *string `json:"updated_at,omitempty" db:"updated_at"`
