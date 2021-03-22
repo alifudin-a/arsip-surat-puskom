60611f52794312137b0c312f40493aec443eb890
alifudin-a <alifudinalfarizi@gmail.com>
2021-03-22 14:47:42 +0700

    fix typo query update

--- domain/query/penerima.go
+++ domain/query/penerima.go
@@ -8,6 +8,6 @@ var DeletePenerima = `DELETE FROM tbl_penerima WHERE id= $1;`
 
 var CreatePenerima = `INSERT INTO tbl_penerima (id_surat, id_pengguna, created_at) VALUES ($1, $2, $3) RETURNING *;`
 
-var UpdatePenerima = `UPDATE tbl_penerima SET id_surat = $1, id_pengguna = $2, created_at = $3 WHERE id = $4 RETURNING *;`
+var UpdatePenerima = `UPDATE tbl_penerima SET id_surat = $1, id_pengguna = $2, updated_at = $3 WHERE id = $4 RETURNING *;`
 
 var IsExistPenerima = `SELECT COUNT(*) FROM tbl_penerima WHERE id = $1;`
