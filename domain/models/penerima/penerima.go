8e6b692a00febc3c1001e700a0b7a6a83d8c5f55
alifudin-a <alifudinalfarizi@gmail.com>
2021-03-25 14:21:50 +0700

    menambahkan field name pada penerima

--- domain/models/penerima/penerima.go
+++ domain/models/penerima/penerima.go
@@ -4,6 +4,7 @@ type Penerima struct {
 	ID         int64   `json:"id" db:"id"`
 	IDSurat    int64   `json:"id_surat" db:"id_surat"`
 	IDPengguna int64   `json:"id_pengguna" db:"id_pengguna"`
+	Name       string  `json:"name,omitempty" db:"name"`
 	CreatedAt  *string `json:"created_at,omitempty" db:"created_at"`
 	UpdatedAt  *string `json:"updated_at,omitempty" db:"updated_at"`
 }
