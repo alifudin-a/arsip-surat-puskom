1e446d9175c1096fa90652e1bdc4485d92058dcb
alifudin-a <alifudinalfarizi@gmail.com>
2021-04-08 16:52:06 +0700

    hapus parameter ke-3

--- domain/query/jenis-surat.go
+++ domain/query/jenis-surat.go
@@ -8,6 +8,6 @@ var DeleteJenisSurat = `DELETE FROM tbl_jenis_surat WHERE id = $1;`
 
 var IsExistJenisSurat = `SELECT COUNT(*) FROM tbl_jenis_surat WHERE id = $1;`
 
-var CreateJenisSurat = `INSERT INTO tbl_jenis_surat (name, created_at) VALUES ($1, $2, $3) RETURNING *;`
+var CreateJenisSurat = `INSERT INTO tbl_jenis_surat (name, created_at) VALUES ($1, $2) RETURNING *;`
 
 var UpdateJenisSurat = `UPDATE tbl_jenis_surat SET name = $1, updated_at = $2 WHERE id = $3 RETURNING *;`
