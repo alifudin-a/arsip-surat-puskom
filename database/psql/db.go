e614022bb94466eb6fb6d3c3a5dfa039ea575ff4
alifudin-a <alifudinalfarizi@gmail.com>
2021-04-17 17:43:28 +0700

    menambahkan comment

--- database/psql/db.go
+++ database/psql/db.go
@@ -25,6 +25,7 @@ func OpenDB() {
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
 		"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
+	// Initialise a new connection pool
 	db, err := sqlx.Open("postgres", psqlInfo)
 	if err != nil {
 		log.Println("An error occured while connecting to database: ", err)
@@ -32,9 +33,18 @@ func OpenDB() {
 		log.Printf("Connected to database %s\n", dbname)
 	}
 
-	// database pooling
+	// Set the maximum number of concurrently idle connections to 10. Setting this
+	// to less than or equal to 0 will mean that no idle connections are retained.
 	db.DB.SetMaxIdleConns(10)
+
+	// Set the maximum number of concurrently open connections (in-use + idle) to 10.
+	// Setting this to less than or equal to 0 will mean there is no
+	// maximum limit (which is also the default setting).
 	db.DB.SetMaxOpenConns(10)
+
+	// Set the maximum lifetime of a connection to 5 minute. Setting it to 0
+	// means that there is no maximum lifetime and the connection is reused
+	// forever (which is the default behavior).
 	db.DB.SetConnMaxLifetime(time.Minute * 5)
 
 	DB = db
