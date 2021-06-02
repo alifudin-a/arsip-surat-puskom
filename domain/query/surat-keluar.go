76114402e2a69797a00a51292608a0f57c749685
alifudin-a <alifudinalfarizi@gmail.com>
2021-06-02 11:33:36 +0700

    menambahkan field upload

--- domain/query/surat-keluar.go
+++ domain/query/surat-keluar.go
@@ -196,9 +196,10 @@ var CreateSuratKeluar = `
 			perihal,
 			id_jenis ,
 			keterangan ,
-			created_at 
+			created_at,
+			upload 
 		)
-		VALUES ( $1, $2, $3, $4, $5, $6, $7 ) RETURNING *;`
+		VALUES ( $1, $2, $3, $4, $5, $6, $7, $8 ) RETURNING *;`
 
 var UpdateSuratKeluar = `
 	UPDATE 
